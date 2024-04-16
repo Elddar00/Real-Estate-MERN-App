73d4f0ae1be399ee35fb04321d891c84da2ea720
Elddar00 <eldaridrizovic00@gmail.com>
2024-04-16 03:06:50 +0200

    Prisma MongoDB - relation

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,19 +1,42 @@
 import bcrypt from "bcrypt";
+import prisma from "../lib/prisma.js";
 
 export const register = async (req, res) => {
   const { username, email, password } = req.body;
 
-  // HASH PASSWORD
+  try {
+    // HASH PASSWORD
 
-  const hashedPassword = await bcrypt.hash(password, 10);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-  console.log(hashedPassword);
+    console.log(hashedPassword);
 
-  // CREATE A NEW USER AND SAVE TO DB
+    // CREATE A NEW USER AND SAVE TO DB
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    console.log(newUser);
+
+    res.status(201).json({ message: "User created successfully" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to create user" });
+  }
 };
 
 export const login = (req, res) => {
-  //db operations
+  const { username, password } = req.body;
+
+  // CHECK IF THE USER EXIST
+
+  // CHECK IF THE PASSWORD IS CORRECT
+
+  // GENERATE COOKIE TOKEN AND SEND TO USER
 };
 
 export const logout = (req, res) => {
