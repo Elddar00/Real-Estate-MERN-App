465092300fcf6aeabe0031f211fe63e53206a29a
Elddar00 <eldaridrizovic00@gmail.com>
2024-04-21 21:17:51 +0200

    Express.js Prisma CRUD - User Information Update

--- api/app.js
+++ api/app.js
@@ -3,8 +3,9 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import postRoute from "./routes/post.route.js";
 import testRoute from "./routes/test.route.js";
-
 import authRoute from "./routes/auth.route.js";
+import userRoute from "./routes/user.route.js";
+
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -22,8 +23,9 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
 app.use("/api/test", testRoute);
 
 app.listen(8800, () => {
