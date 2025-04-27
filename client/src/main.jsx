c6dcdf743eae32aae6400bebf922747e4c38f33a
Elddar00 <eldaridrizovic00@gmail.com>
2025-04-27 23:09:18 +0200

    Fix - Leaflet Marker Icon

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,6 +6,17 @@ import "./index.scss";
 import { AuthContextProvider } from "./context/AuthContext.jsx";
 import { SocketContextProvider } from "./context/SocketContext.jsx";
 
+import L from "leaflet";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthContextProvider>
